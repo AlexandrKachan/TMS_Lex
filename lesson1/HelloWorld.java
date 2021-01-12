b54295cc42e6862754fa204f650fc63a01d23009
Alexandr Kachan <kachan.alexandr@gmail.com>
2021-01-13 00:33:31 +0300

    hello world example

---
+++ lesson1/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+
+	public static void main (String ... args)throws Exception {
+		while(true) {
+			System.out.println("Helo TMS group world!!!");
+			Thread.sleep(1000);
+			}
+			
+		
+	}
+
+
+}
